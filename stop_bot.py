bb19942d7edc7888c73ca94994f1d652d86da3b0
Ben Finley <bben10hi@gmail.com>
2022-03-06 14:10:28 -0500

    updated to use send message command from main

--- stop_bot.py
+++ stop_bot.py
@@ -1,10 +1,10 @@
-def command(update, context, updater, admin_chat_id):
+import main
+
+def command(update, updater, admin_chat_id):
     """
         stop the bot through a telegram command
     """
-    print(update.effective_chat.id)
     if update.effective_chat.id == admin_chat_id:
-        updater.bot.sendMessage(
-            chat_id=update.effective_chat.id, text='Shuting Down')
+        main.send_message(admin_chat_id, 'Shuting Down')
         updater.stop()
         updater.is_idle = False
