7ec4d3a7eb53a5372ee1b88012ffa71f08722a59
Ben Finley <bben10hi@gmail.com>
2022-03-21 18:05:21 -0400

    added api call for F1 api

--- api_checks.py
+++ api_checks.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import xmltodict
 from dateutil.relativedelta import relativedelta
 
 def schedule_call(url_ending):
@@ -35,6 +36,14 @@ def standings_call():
     final_url = f'https://statsapi.web.nhl.com/api/v1/standings'
     request = requests.get(final_url)
     json = request.json()
+    return json
+
+
+def f1_current(url_ending):
+    base_url = f'http://ergast.com/api/f1/current'
+    final_url = base_url + url_ending
+    request = requests.get(final_url)
+    json = xmltodict.parse(request.content)
     return json
 
 def season(todays_date):
