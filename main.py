8b257d2dcf57612280bf0faf3eac07007ba9ef71
Ben Finley <bben10hi@gmail.com>
2022-03-21 14:18:46 -0400

    updated sendmessages to handle sending charts as images instead of text

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import datetime, date
 import pytz
 
 # my scripts
-from handle_messages import send_message
+from handle_messages import send
 import start_bot
 import help_bot
 import setup_user
@@ -28,7 +28,7 @@ import stop_bot
 import unknown_message
 
 # gets the bot token from remote database so the code can be made public
-bot_token_dataframe = pd.read_csv((os.path.join(os.path.dirname(os.getcwd()), "TelegramBotTokens.csv")))
+bot_token_dataframe = pd.read_csv((os.path.join(os.path.dirname(os.getcwd() + "/hockeybot-v2"), "TelegramBotTokens.csv")))
 bot_index = int(bot_token_dataframe.index[bot_token_dataframe['Bot Name'] == 'Hockey Bot testing'].values)
 bot_token = str(bot_token_dataframe.loc[[bot_index], ['Bot Token']].values).strip("'[]")
 bot = Bot(bot_token)
@@ -37,9 +37,9 @@ bot = Bot(bot_token)
 updater = Updater(bot_token, use_context=True)
 
 # set the database paths so the bot works on any OS.
-chat_database_win = PureWindowsPath('.\Database\ChatDatabase.csv')
-teams_database_win = PureWindowsPath('.\Database\TeamNames.csv')
-todays_games_database_win = PureWindowsPath('.\Database\\todaysgames.csv')
+chat_database_win = PureWindowsPath('.\hockeybot-v2\Database\ChatDatabase.csv')
+teams_database_win = PureWindowsPath('.\hockeybot-v2\Database\TeamNames.csv')
+todays_games_database_win = PureWindowsPath('.\hockeybot-v2\Database\\todaysgames.csv')
 
 # OS-independent paths
 chat_database = Path(chat_database_win)
@@ -58,13 +58,13 @@ starting = True
 def start(update, context):
     chat_id=update.effective_chat.id
     message = start_bot.message()
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 # returns a list of commands and bot info
 def help(update, context):
     chat_id=update.effective_chat.id
     message = help_bot.message()
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 # sets up the teams a user is following and their notification status
 def user_setup(update, context):
@@ -74,7 +74,7 @@ def user_setup(update, context):
 def user_status(update, context):
     chat_id = update.effective_chat.id
     message = status.message(chat_id, chat_database, teams_database)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 def user_remove(update, context):
     remove_user.remove(update, context, updater, chat_database)
@@ -82,53 +82,53 @@ def user_remove(update, context):
 def check_game(update, context):
     chat_id = update.effective_chat.id
     message = game_check_request.message(chat_database, chat_id, todays_date, dst_check)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 def check_next_game(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[10:].lower()
     message = next_game_check.message(user_request, teams_database, dst_check, todays_date)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 def check_last_game(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[10:].lower()
     message = last_game_check.message(user_request, teams_database)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
     
 # returns the division standings for one or all divisions as requested by the user
 def check_standings(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[11:].lower()
     messages = standings.message(user_request)
-    send_message(updater, chat_id, messages)
+    send(updater, chat_id, messages)
 
 # returns the roster for a requested team
 def check_roster(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[8:].lower()
     message = roster.message(user_request, teams_database)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 # returns a players name and position based on their jersey number
 def check_player(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[8:].lower()
     message = player.message(user_request, teams_database)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 # returns the stats or a player or team as requested by the user
 def check_stats(update, context):
     chat_id=update.effective_chat.id
     user_request = update.message.text[7:].lower()
     message = stats.message(user_request, teams_database)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 # returns the days since the flyers and pens have won the cup, LGP!
 def check_cupcheck(update, context):
     chat_id=update.effective_chat.id
     message = cupcheck.message(todays_date)
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 #### ADMIN COMMANDS ###
 
@@ -152,7 +152,7 @@ def stop(update, context):
 def unknown(update, context):
     chat_id=update.effective_chat.id
     message = unknown_message.message()
-    send_message(updater, chat_id, message)
+    send(updater, chat_id, message)
 
 ### Automation Functions ###
 def start_notifications(starting):
