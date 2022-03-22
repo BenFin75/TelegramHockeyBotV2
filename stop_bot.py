204c0f9a1f510eb2a3f03ffea6766a68344a1c4f
Ben Finley <bben10hi@gmail.com>
2022-03-22 10:51:43 -0400

    moved chat id check to main function

--- stop_bot.py
+++ stop_bot.py
@@ -1,10 +1,8 @@
 from handle_messages import send
 
-def command(update, updater, admin_chat_id):
+def command(updater):
     """
         stop the bot through a telegram command
     """
-    if update.effective_chat.id == admin_chat_id:
-        send(admin_chat_id, 'Shuting Down')
-        updater.stop()
-        updater.is_idle = False
+    updater.stop()
+    updater.is_idle = False
