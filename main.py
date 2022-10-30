4c7f0b0e0f2ce9361205bde1b36a9a636e8a5c55
Ben Finely <benhinden@gmail.com>
2022-10-30 14:22:41 -0400

    changed bot key to main

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ if admin_chat_id:
 else:
     sys.exit("no admin chat id found. Please set $BOT_ADMIN_ID")
     
-bot_token = os.getenv("HOCKEY_BOT_TESTING_KEY")
+bot_token = os.getenv("HOCKEY_BOT_KEY")
 if bot_token:
     bot = Bot(bot_token)
 else:
